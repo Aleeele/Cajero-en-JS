3069f0b507e2f703b002c98c4b71aece66e81aa8
Aleeele <120615389+Aleeele@users.noreply.github.com>
2023-03-03 21:21:46 -0600

    Add files via upload

---
+++ Cajero en JS/main.js
@@ -0,0 +1,31 @@
+function login(form){
+    if(form.u.value == "Owen"){
+        if(form.c.value == "123"){
+        location="principal.html";    
+    }
+    else {
+        alert("Error de Contraseña");
+    }
+    }else {
+        alert("Error de Usuraio");
+    }
+    if(form.u.value == "Eduardo"){
+        if(form.c.value == "321"){
+        location="principal.html";
+    }else{
+        alert("Error de Contraseña");
+    }
+    }else {
+        alert("Error de Usuraio");
+    }
+    }
+    if(form.u.value == "Juan"){
+        if(form.c.value == "231"){
+        location="principal.html";
+    }else{
+        alert("Error de Contraseña");
+    }
+    }else {
+        alert("Error de Usuraio");
+    }
+
